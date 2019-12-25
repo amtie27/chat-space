710124526ba00911c75c86a38ad990e7335b7458
amtie27 <yuki.sax.ramt@gmail.com>
2019-12-25 21:15:42 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,25 +61,6 @@ $(function(){
         return false;
       });
   });
-$('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    console.log("test");
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.messages').append(html);
-       $('form')[0].reset();
-     })
-   })
    var reloadMessages = function() {
        //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
        if (document.location.href.match(/\/groups\/\d+\/messages/)) {
